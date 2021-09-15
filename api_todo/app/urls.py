844402e0aa3b64afbe9bfdf0f7338f1fca739a9c
Alx-404 <alexanderreis70@gmail.com>
2021-09-15 13:55:47 -0300

    uploading all files

---
+++ api_todo/app/urls.py
@@ -0,0 +1,19 @@
+
+'''
+from app.views import TodoListAndCreate,TodoDetailChangeAndDelete
+from django.urls import path
+
+urlpatterns = [
+    path('',TodoListAndCreate.as_view()),
+    path('<int:pk>/',TodoDetailChangeAndDelete.as_view()),
+]
+'''
+
+from django.urls.resolvers import URLPattern
+from app.views import TodoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'',TodoViewSet)
+urlpatterns = router.urls
+
